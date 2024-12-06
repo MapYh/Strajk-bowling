ee9276681dedb81a32839e9d4f03e6431cefc17c
MapYh <145336421+MapYh@users.noreply.github.com>
2024-12-06 20:29:53 +0100

    Added some text changes.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,13 +1,14 @@
 import { render, screen } from "@testing-library/react";
-import { MemoryRouter, Route, Routes } from "react-router-dom";
 
 import App from "./App"; // Your top-level component
 
-test("renders nested components with React Router", () => {
-  render(<App />);
-
-  const booking_text = screen.getAllByText("Booking");
-  // Verify the default UI
-  console.log(booking_text[0]);
-  expect(booking_text[0]).toBeInTheDocument();
+describe("renders nested components with React Router", () => {
+  it("Should check that all the text is diplayed correctly when the first page is loaded in.", () => {
+    render(<App />);
+    const booking_text = screen.getAllByText("Booking");
+    // Verify the default UI
+    console.log(booking_text[0]);
+    expect(booking_text[0]).toBeInTheDocument();
+    expect(booking_text[1]).toBeInTheDocument();
+  });
 });
