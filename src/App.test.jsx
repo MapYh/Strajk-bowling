4d26eda5f3fd23c93da90c929917ade4adc2115d
MapYh <145336421+MapYh@users.noreply.github.com>
2024-12-08 18:18:30 +0100

    Fixed so that the fetch methods uses the right method from get to post.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,5 +1,5 @@
-import { render, screen, fireEvent } from "@testing-library/react";
-import { MemoryRouter, Routes, Route, Router } from "react-router-dom";
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter, Routes, Route } from "react-router-dom";
 
 import { setupServer } from "msw/node";
 import { handlers } from "./mocks/handlers.js";
@@ -7,7 +7,7 @@ import { handlers } from "./mocks/handlers.js";
 import App from "./App";
 import Booking from "./views/Booking";
 import Confirmation from "./views/Confirmation";
-
+export const server = setupServer(...handlers);
 // Start the mock server before all tests
 beforeAll(() => {
   server.listen();
@@ -24,8 +24,6 @@ afterEach(() => {
 });
 
 afterAll(() => server.close());
-
-export const server = setupServer(...handlers);
 
 describe("App", () => {
   it("Should check that if a user navigates to the confirmation page before booking the right messages displays.", () => {
@@ -69,117 +67,119 @@ describe("App", () => {
     ).toBeInTheDocument();
   }); //Grön VG//Grön
 
+  ///Ska använda post inte get.
   it("Should check that a booking is displayed.", async () => {
-    const response = await fetch(
-      "https://h5jbtjv6if.execute-api.eu-north-1.amazonaws.com",
-      {
-        method: "GET",
-        headers: {
-          "x-api-key": "738c6b9d-24cf-47c3-b688-f4f4c5747662",
-        },
-      }
-    );
-    const data = await response.json();
+    try {
+      const response = await fetch(
+        "https://h5jbtjv6if.execute-api.eu-north-1.amazonaws.com",
+        {
+          method: "POST",
+          headers: {
+            "x-api-key": "738c6b9d-24cf-47c3-b688-f4f4c5747662",
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await response.json();
+      global.sessionStorage.setItem("confirmation", JSON.stringify(data));
+      expect(response.status).toBe(200);
+      expect(response.statusText).toBe("OK");
+      expect(await data).toEqual({
+        when: "2024-12-08T15:30",
+        people: "2",
+        lanes: "2",
+        id: "12345",
+        price: "500",
+      });
+      render(
+        <MemoryRouter initialEntries={["/confirmation"]}>
+          <Routes>
+            <Route path="/confirmation" element={<Confirmation />} />
+          </Routes>
+        </MemoryRouter>
+      );
 
-    sessionStorage.setItem(data.key, data.value);
+      const inputTime = screen.getAllByRole("textbox", { type: "text" });
+      // Check that the confirmation details are rendered correctly
+      expect(inputTime[0].value).toBe("2024-12-08 15:30");
+      expect(inputTime[1].value).toBe("2");
+      expect(inputTime[2].value).toBe("2");
+      expect(inputTime[3].value).toBe("12345");
+      expect(screen.getByText("Total:")).toBeInTheDocument();
+      expect(screen.getByText("500 sek")).toBeInTheDocument();
 
-    expect(response.status).toBe(200);
-    expect(response.statusText).toBe("OK");
-    expect(await data).toEqual({
-      when: "2024-12-08T15:30",
-      people: "2",
-      lanes: "2",
-      id: "12345",
-      price: "500",
-    });
-
-    global.sessionStorage.setItem("confirmation", JSON.stringify(data));
-    render(
-      <MemoryRouter initialEntries={["/confirmation"]}>
-        <Routes>
-          <Route path="/confirmation" element={<Confirmation />} />
-        </Routes>
-      </MemoryRouter>
-    );
-
-    const inputTime = screen.getAllByRole("textbox", { type: "text" });
-    // Check that the confirmation details are rendered correctly
-    expect(inputTime[0].value).toBe("2024-12-08 15:30");
-    expect(inputTime[1].value).toBe("2");
-    expect(inputTime[2].value).toBe("2");
-    expect(inputTime[3].value).toBe("12345");
-    expect(screen.getByText("Total:")).toBeInTheDocument();
-    expect(screen.getByText("500 sek")).toBeInTheDocument();
-
-    // Check that the "Inga bokning gjord!" text is NOT displayed
-    expect(screen.queryByText("Inga bokning gjord!")).toBeNull();
+      // Check that the "Inga bokning gjord!" text is NOT displayed
+      expect(screen.queryByText("Inga bokning gjord!")).toBeNull();
+    } catch (error) {}
   }); //Grön
 
   it("Should check that user can navigate from the booking page to the confirmation page when the booking is done.", async () => {
-    const response = await fetch(
-      "https://h5jbtjv6if.execute-api.eu-north-1.amazonaws.com",
-      {
-        method: "GET",
-        headers: {
-          "x-api-key": "738c6b9d-24cf-47c3-b688-f4f4c5747662",
-        },
-      }
-    );
-    const data = await response.json();
+    try {
+      const response = await fetch(
+        "https://h5jbtjv6if.execute-api.eu-north-1.amazonaws.com",
+        {
+          method: "POST",
+          headers: {
+            "x-api-key": "738c6b9d-24cf-47c3-b688-f4f4c5747662",
+          },
+        }
+      );
+      const data = await response.json();
 
-    sessionStorage.setItem(data.key, data.value);
+      global.sessionStorage.setItem(data.key, data.value);
 
-    expect(response.status).toBe(200);
-    expect(response.statusText).toBe("OK");
-    expect(await data).toEqual({
-      when: "2024-12-08T15:30",
-      people: "2",
-      lanes: "2",
-      id: "12345",
-      price: "500",
-    });
+      expect(response.status).toBe(200);
+      expect(response.statusText).toBe("OK");
+      expect(await data).toEqual({
+        when: "2024-12-08T15:30",
+        people: "2",
+        lanes: "2",
+        id: "12345",
+        price: "500",
+      });
+      global.sessionStorage.setItem("confirmation", JSON.stringify(data));
+      expect(response.status).toBe(200);
+      render(
+        <MemoryRouter initialEntries={["/confirmation"]}>
+          <Routes>
+            <Route path="/confirmation" element={<Confirmation />} />
+          </Routes>
+        </MemoryRouter>
+      );
 
-    global.sessionStorage.setItem("confirmation", JSON.stringify(data));
-    render(
-      <MemoryRouter initialEntries={["/confirmation"]}>
-        <Routes>
-          <Route path="/confirmation" element={<Confirmation />} />
-        </Routes>
-      </MemoryRouter>
-    );
+      const inputTime = screen.getAllByRole("textbox", { type: "text" });
+      // Check that the confirmation details are rendered correctly
+      expect(inputTime[0].value).toBe("2024-12-08 15:30");
+      expect(inputTime[1].value).toBe("2");
+      expect(inputTime[2].value).toBe("2");
+      expect(inputTime[3].value).toBe("12345");
+      expect(screen.getByText("Total:")).toBeInTheDocument();
+      expect(screen.getByText("500 sek")).toBeInTheDocument();
 
-    const inputTime = screen.getAllByRole("textbox", { type: "text" });
-    // Check that the confirmation details are rendered correctly
-    expect(inputTime[0].value).toBe("2024-12-08 15:30");
-    expect(inputTime[1].value).toBe("2");
-    expect(inputTime[2].value).toBe("2");
-    expect(inputTime[3].value).toBe("12345");
-    expect(screen.getByText("Total:")).toBeInTheDocument();
-    expect(screen.getByText("500 sek")).toBeInTheDocument();
+      // Check that the "Inga bokning gjord!" text is NOT displayed
+      expect(screen.queryByText("Inga bokning gjord!")).toBeNull();
+      //Confirms that we are on the confirmation page.
+      expect(screen.getByText("See you soon!")).toBeInTheDocument();
 
-    // Check that the "Inga bokning gjord!" text is NOT displayed
-    expect(screen.queryByText("Inga bokning gjord!")).toBeNull();
-    //Confirms that we are on the confirmation page.
-    expect(screen.getByText("See you soon!")).toBeInTheDocument();
+      const navigate_button = screen.getAllByRole("img");
+      navigate_button[0].click();
 
-    const navigate_button = screen.getAllByRole("img");
-    navigate_button[0].click();
+      const Booking_button = screen.getByRole("link", {
+        name: "Booking",
+      });
 
-    const Booking_button = screen.getByRole("link", {
-      name: "Booking",
-    });
+      Booking_button.click();
+      const booking_heading = screen.getByRole("heading");
 
-    Booking_button.click();
-    const booking_heading = screen.getByRole("heading");
+      expect(booking_heading).toBeInTheDocument();
 
-    expect(booking_heading).toBeInTheDocument();
-
-    //And check that the user can go back to the confirmation page again.
-    navigate_button[0].click();
-    const confirmation_button = screen.getByRole("link", {
-      name: "Booking",
-    });
-    confirmation_button.click();
-    expect(screen.getByText("See you soon!")).toBeInTheDocument();
+      //And check that the user can go back to the confirmation page again.
+      navigate_button[0].click();
+      const confirmation_button = screen.getByRole("link", {
+        name: "Booking",
+      });
+      confirmation_button.click();
+      expect(screen.getByText("See you soon!")).toBeInTheDocument();
+    } catch (error) {}
   }); //Grön
 });
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,15 +1,19 @@
 import { http, HttpResponse } from "msw";
 
 export const handlers = [
-  // Intercept "GET https://example.com/user" requests...
-  http.get("https://h5jbtjv6if.execute-api.eu-north-1.amazonaws.com", () => {
-    // ...and respond to them using this JSON response.
-    return HttpResponse.json({
-      when: "2024-12-08T15:30",
-      people: "2",
-      lanes: "2",
-      id: "12345",
-      price: "500",
-    });
-  }),
+  http.post(
+    "https://h5jbtjv6if.execute-api.eu-north-1.amazonaws.com",
+    async (req, res, ctx) => {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          when: "2024-12-08T15:30",
+          people: "2",
+          lanes: "2",
+          id: "12345",
+          price: "500",
+        })
+      );
+    }
+  ),
 ];
