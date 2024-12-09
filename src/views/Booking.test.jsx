e7cfd2380fe47c25b5fa06f4b0865048d8a8e720
MapYh <145336421+MapYh@users.noreply.github.com>
2024-12-09 20:06:44 +0100

    Added some more tests to the booking component.

--- src/views/Booking.test.jsx
+++ src/views/Booking.test.jsx
@@ -144,4 +144,178 @@ describe("App", () => {
     );
     expect(screen.getByText("See you soon!")).toBeInTheDocument();
   });
+
+  it("A user should be able to enter a shoe size for all bowlers.", async () => {
+    render(
+      <>
+        <MemoryRouter initialEntries={["/Booking"]}>
+          <Routes>
+            <Route path="/booking" element={<Booking />} />
+          </Routes>
+        </MemoryRouter>
+      </>
+    );
+
+    const number = screen.getByLabelText("Number of awesome bowlers");
+    fireEvent.change(number, { target: { value: "5" } });
+    expect(number.value).toBe("5");
+
+    //Click all the shoe buttons.
+    for (let i = 0; i < 5; i++) {
+      fireEvent.click(screen.getByText("+"));
+    }
+    //Enter all the shoe sizes.
+    for (let index = 1; index < 6; index++) {
+      let shoe = screen.getByLabelText(`Shoe size / person ${index}`);
+      fireEvent.change(shoe, { target: { value: `3${index}` } });
+      expect(shoe.value).toBe(`3${index}`);
+    }
+  });
+  it("A user should be able to change the shoe size for all bowlers.", async () => {
+    render(
+      <>
+        <MemoryRouter initialEntries={["/Booking"]}>
+          <Routes>
+            <Route path="/booking" element={<Booking />} />
+          </Routes>
+        </MemoryRouter>
+      </>
+    );
+
+    const number = screen.getByLabelText("Number of awesome bowlers");
+    fireEvent.change(number, { target: { value: "5" } });
+    expect(number.value).toBe("5");
+
+    //Click all the shoe buttons.
+    for (let i = 0; i < 5; i++) {
+      fireEvent.click(screen.getByText("+"));
+    }
+    //Enter all the shoe sizes.
+    for (let index = 1; index < 6; index++) {
+      let shoe = screen.getByLabelText(`Shoe size / person ${index}`);
+      fireEvent.change(shoe, { target: { value: `3${index}` } });
+      expect(shoe.value).toBe(`3${index}`);
+    }
+    for (let index = 1; index < 6; index++) {
+      let shoe = screen.getByLabelText(`Shoe size / person ${index}`);
+      fireEvent.change(shoe, { target: { value: `3${index + 2}` } });
+      expect(shoe.value).toBe(`3${index + 2}`);
+    }
+  });
+
+  it("Should check that the correct amount players and shoes is equal.", async () => {
+    render(
+      <>
+        <MemoryRouter initialEntries={["/Booking"]}>
+          <Routes>
+            <Route path="/booking" element={<Booking />} />
+          </Routes>
+        </MemoryRouter>
+      </>
+    );
+
+    //Used to fill in the booking info.
+    const number = screen.getByLabelText("Number of awesome bowlers");
+    const number_of_lanes = screen.getByLabelText("Number of lanes");
+    const dateInput = screen.getByLabelText(/Date/i);
+    const timeInput = screen.getByLabelText(/Time/i);
+    fireEvent.change(dateInput, { target: { value: "2024-12-08" } });
+    fireEvent.change(timeInput, { target: { value: "14:30" } });
+    //Choose the mumber of lanes and bowlers.
+    fireEvent.change(number_of_lanes, { target: { value: "2" } });
+    fireEvent.change(number, { target: { value: "5" } });
+    //Confirmation of the booking numbers
+    expect(dateInput.value).toBe("2024-12-08");
+    expect(timeInput.value).toBe("14:30");
+    expect(number_of_lanes.value).toBe("2");
+    expect(number.value).toBe("5");
+
+    //Click all the shoe buttons.
+    for (let i = 0; i < 4; i++) {
+      fireEvent.click(screen.getByText("+"));
+    }
+    //Enter all the shoe sizes except one.
+    for (let index = 1; index < 5; index++) {
+      let shoe = screen.getByLabelText(`Shoe size / person ${index}`);
+      fireEvent.change(shoe, { target: { value: `3${index}` } });
+      expect(shoe.value).toBe(`3${index}`);
+    }
+    const strike_button = screen.getAllByRole("button");
+    fireEvent.click(strike_button[5]);
+    expect(
+      screen.getByText("Antalet skor måste stämma överens med antal spelare")
+    ).toBeInTheDocument();
+  });
+  it("Should check that all the bowlers have entered a shoe size.", async () => {
+    render(
+      <>
+        <MemoryRouter initialEntries={["/Booking"]}>
+          <Routes>
+            <Route path="/booking" element={<Booking />} />
+          </Routes>
+        </MemoryRouter>
+      </>
+    );
+    //Used to fill in the booking info.
+    const number = screen.getByLabelText("Number of awesome bowlers");
+    const number_of_lanes = screen.getByLabelText("Number of lanes");
+    const dateInput = screen.getByLabelText(/Date/i);
+    const timeInput = screen.getByLabelText(/Time/i);
+    fireEvent.change(dateInput, { target: { value: "2024-12-08" } });
+    fireEvent.change(timeInput, { target: { value: "14:30" } });
+    //Choose the mumber of lanes and bowlers.
+    fireEvent.change(number_of_lanes, { target: { value: "2" } });
+    fireEvent.change(number, { target: { value: "5" } });
+    //Confirmation of the booking numbers
+    expect(dateInput.value).toBe("2024-12-08");
+    expect(timeInput.value).toBe("14:30");
+    expect(number_of_lanes.value).toBe("2");
+    expect(number.value).toBe("5");
+
+    //Click all the shoe buttons.
+    for (let i = 0; i < 5; i++) {
+      fireEvent.click(screen.getByText("+"));
+    }
+    //Enter all the shoe sizes except one.
+    for (let index = 1; index < 5; index++) {
+      let shoe = screen.getByLabelText(`Shoe size / person ${index}`);
+      fireEvent.change(shoe, { target: { value: `3${index}` } });
+      expect(shoe.value).toBe(`3${index}`);
+    }
+    const strike_button = screen.getAllByRole("button");
+    fireEvent.click(strike_button[6]);
+    expect(
+      screen.getByText("Alla skor måste vara ifyllda")
+    ).toBeInTheDocument();
+  });
+
+  it("Should check that is a overview of all the shoes before the booking is confirmed.", async () => {
+    render(
+      <>
+        <MemoryRouter initialEntries={["/Booking"]}>
+          <Routes>
+            <Route path="/booking" element={<Booking />} />
+          </Routes>
+        </MemoryRouter>
+      </>
+    );
+
+    const number = screen.getByLabelText("Number of awesome bowlers");
+    fireEvent.change(number, { target: { value: "5" } });
+    expect(number.value).toBe("5");
+
+    //Click all the shoe buttons.
+    for (let i = 0; i < 5; i++) {
+      fireEvent.click(screen.getByText("+"));
+    }
+    //Enter all the shoe sizes except one.
+    for (let index = 1; index < 5; index++) {
+      let shoe = screen.getByLabelText(`Shoe size / person ${index}`);
+      fireEvent.change(shoe, { target: { value: `3${index}` } });
+      expect(shoe.value).toBe(`3${index}`);
+    }
+
+    const shoes = screen.getAllByText("-");
+    expect(shoes.length).toBe(5);
+  });
 });
